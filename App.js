76416850d25c3d09a0f9125585fae03d08c65b4c
TesfaMaisa <bhm236@myy.haaga-helia.fi>
2025-09-01 20:08:45 +0300

    Korjattu

--- App.js
+++ App.js
@@ -10,26 +10,27 @@ export default function App() {
 
  const [text,setText] = useState("")
 
- const [random, setRandom] = useState(null)
+ const [random] = useState(randomNumber())
 
  const [counter,setCounter] = useState(1)
 
+      function randomNumber(){ 
+        return Math.floor(Math.random() * 100) + 1
+      };
+
+
+
  function painike(i){
-   const randomNumber = Math.floor(Math.random() * 100) + 1
-   setRandom("Oikea vastaus oli " + randomNumber)
-     if(i < 1 || i > 100){
-     alert("Arvaa numero väliltä 1 - 100!")
-     setCounter(1)
-     setText("")
-     setRandom("")
+  if(i < 1 || i > 100){
+    alert("Kirjoita numero väliltä 1 - 100!")
+    setText("")
   }
-   else if(i == randomNumber){
+    else if(i == random){
      alert("Arvasit oikein! Siihen meni " + counter + " yritystä!")
      setCounter(1)
      setText("")
-     setRandom("")
 
-    }else if (i > randomNumber){
+    }else if (i > random){
       setText('Arvaus ' + i  + ' oli liian paljon')
       setCounter(counter + 1)
 
@@ -42,7 +43,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{fontWeight:'bold', color:'lightblue',fontSize:25}}> {info} {"\n"} </Text> 
-      <Text style={{ fontWeight:'bold', color:'white',fontSize:20}}> {random} {"\n"}</Text> 
       <Text style={{fontWeight:'bold', color:'white',fontSize:15}}> {text} {"\n"} </Text> 
       <TextInput keyboardType='numeric' returnKeyType='done' placeholder='Kirjoita luku' placeholderTextColor={'grey'} style={{borderBottomWidth:2, color:'white'}} onChangeText={number => setNumber(number)} value={Number(number)} /> 
       <Button title='Arvaa' onPress={() => painike(number)}/>
